a7cfbad2cf76685a9b6a96f346e62a2a92abb63d
David Cournapeau <cournape@gmail.com>
2013-07-26 09:15:04 -0400

    FEAT: add model for S3 static index entry.

---
+++ okonomiyaki/tests/test_enpkg_models.py
@@ -0,0 +1,90 @@
+import json
+import os
+import unittest
+
+import os.path as op
+
+from okonomiyaki.models.enpkg import EnpkgS3IndexEntry
+
+DATA_DIR = op.join(op.dirname(__file__), "data")
+
+__st = os.stat(op.join(DATA_DIR, "ets-4.3.0-3.egg"))
+ETS_MTIME = __st.st_mtime
+ETS_SIZE = __st.st_size
+
+class TestEnpkgS3IndexEntry(unittest.TestCase):
+    def test_from_egg(self):
+        path = op.join(DATA_DIR, "ets-4.3.0-3.egg")
+
+        index_entry = EnpkgS3IndexEntry.from_egg(path)
+        self.assertEqual(index_entry.build, 3)
+        self.assertEqual(index_entry.name, "ets")
+        self.assertEqual(index_entry.version, "4.3.0")
+
+    def test_to_json(self):
+        r_data = dict(
+            build=3L,
+            md5=u"78ce2b9ebc88e3ed81cb9c0aa4eb8c87",
+            mtime=ETS_MTIME,
+            egg_basename=u'ets',
+            name=u'ets',
+            packages=[
+              'apptools 4.2.0-2',
+              'blockcanvas 4.0.3-1',
+              'casuarius 1.1-1',
+              'chaco 4.3.0-2',
+              'codetools 4.1.0-2',
+              'enable 4.3.0-5',
+              'enaml 0.6.8-2',
+              'encore 0.3-1',
+              'envisage 4.3.0-2',
+              'etsdevtools 4.0.2-1',
+              'etsproxy 0.1.2-1',
+              'graphcanvas 4.0.2-1',
+              'mayavi 4.3.0-3',
+              'pyface 4.3.0-2',
+              'scimath 4.1.2-2',
+              'traits 4.3.0-2',
+              'traitsui 4.3.0-2'],
+            product='commercial',
+            python=u'2.7',
+            size=10027L,
+            type='egg',
+            version=u'4.3.0')
+
+        path = op.join(DATA_DIR, "ets-4.3.0-3.egg")
+
+        index_entry = EnpkgS3IndexEntry.from_egg(path)
+        self.maxDiff = 2048
+        self.assertEqual(r_data, index_entry.to_dict())
+
+    def test_from_data(self):
+        json_data = """\
+{
+    "build": 1,
+    "md5": "efaf1e95fe51ffc7b07219668d4d5a29",
+    "mtime": 1.0,
+    "egg_basename": "numpy",
+    "osdist": null,
+    "packages": [
+      "MKL 10.3-1"
+    ],
+    "python": "2.7",
+    "size": 3251940,
+    "type": "egg",
+    "version": "1.7.1"
+}
+"""
+        index_entry = EnpkgS3IndexEntry.from_data(json.loads(json_data))
+
+        # Test the value that may be null in the json
+        self.assertEqual(index_entry.osdist, "")
+
+        self.assertEqual(index_entry.name, u"numpy")
+
+    def test_cased_egg_basename(self):
+        path = op.join(DATA_DIR, "Cython-0.19.1-1.egg")
+
+        index_entry = EnpkgS3IndexEntry.from_egg(path)
+        self.assertEqual(index_entry.name, u"cython")
+        self.assertEqual(index_entry.egg_basename, u"Cython")
---
+++ okonomiyaki/utils/__init__.py
@@ -0,0 +1,29 @@
+import hashlib
+
+def compute_md5(path):
+    """Compute the md5 checksum of the given path.
+
+    Avoids reading the whole file in RAM, and computes the md5 in chunks.
+    
+    Parameters
+    ----------
+    path: str or file object
+        If a string, assumed to be the path to the file to be checksumed. If a
+        file object, checksum will start at the current file position.
+    """
+    block_size = 256 * 1024
+    m = hashlib.md5()
+
+    def _compute_checksum(fp):
+        while True:
+            data = fp.read(block_size)
+            m.update(data)
+            if len(data) < block_size:
+                break
+        return m.hexdigest()
+
+    if isinstance(path, basestring):
+        with open(path, "rb") as fp:
+            return _compute_checksum(fp)
+    else:
+        return _compute_checksum(path)
