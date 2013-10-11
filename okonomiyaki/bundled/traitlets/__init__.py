b899691d10b76558e88f4f77dd4f0cbc305f0e9c
David Cournapeau <cournape@gmail.com>
2013-10-11 12:26:29 +0100

    ENH: update traitlets to newest version.

--- okonomiyaki/bundled/traitlets/__init__.py
+++ okonomiyaki/bundled/traitlets/__init__.py
@@ -1,6 +1,9 @@
-__version__ = "0.1.0.dev1"
+try:
+    from _version import version as __version__
+except ImportError:
+    __version__ = "no-built"
 
 from ._implementation import HasTraits, MetaHasTraits, TraitType, Any, CBytes, Dict, \
     Int, Long, Integer, Float, Complex, Bytes, Unicode, TraitError, \
     Undefined, Type, This, Instance, TCPAddress, List, Tuple, \
-    ObjectName, DottedObjectName, CRegExp, Enum
+    ObjectName, DottedObjectName, CRegExp, Enum, Bool, CBool
