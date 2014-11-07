9b2475c3d4373486de2923d6a51c8f2c4662e6db
David Cournapeau <cournape@gmail.com>
2014-11-07 17:45:10 +0000

    MAINT: pypy on travis-ci needs unittest2 as well.

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 set -e
 
-if [ "${TRAVIS_PYTHON_VERSION}" == "2.6" ] || [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]; then
+if [ "${TRAVIS_PYTHON_VERSION}" == "2.6" ] || \
+    [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ] || \
+    [ "${TRAVIS_PYTHON_VERSION}" == "pypy" ]; then
     pip install -rdev_requirements2.txt;
 else
     pip install -rdev_requirements.txt;
