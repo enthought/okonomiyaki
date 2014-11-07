3aadf9752adffd42ad0d1cabcc6ab8e7ecd34810
David Cournapeau <cournape@gmail.com>
2014-11-07 17:38:35 +0000

    MAINT: fix travis-ci script.

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -3,12 +3,6 @@ set -e
 
 if [ "${TRAVIS_PYTHON_VERSION}" == "2.6" ] || [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ]; then
     pip install -rdev_requirements2.txt;
-elif [[ "${TRAVIS_PYTHON_VERSION}" = "3.4" ]]; then
-    deactivate;
-    python3.4 -m venv ~/virtualenv/python3.4;
-    source ~/virtualenv/python3.4/bin/activate;
-    python -m ensurepip;
-    python -m pip install -r dev_requirements.txt;
 else
     pip install -rdev_requirements.txt;
 fi
