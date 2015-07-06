223487ef07972d3ef3073e267f2463211985686a
David Cournapeau <cournape@gmail.com>
2015-07-06 10:37:55 +0100

    MAINT: release 0.9.0b1.

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ MAJOR = 0
 MINOR = 9
 MICRO = 0
 
-IS_RELEASED = False
+IS_RELEASED = True
 
-VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+VERSION = '%d.%d.%db1' % (MAJOR, MINOR, MICRO)
 
 
 # Return the git revision as a string
