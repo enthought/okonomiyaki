c25be75728590e6e58d36873e6010b3a05e19353
David Cournapeau <cournape@gmail.com>
2015-08-24 15:47:29 +0100

    FEAT: add compute_sha256 function.

--- okonomiyaki/utils/__init__.py
+++ okonomiyaki/utils/__init__.py
@@ -19,8 +19,25 @@ def compute_md5(path):
         If a string, assumed to be the path to the file to be checksumed. If a
         file object, checksum will start at the current file position.
     """
+    return _compute_hash(path, hashlib.md5())
+
+
+def compute_sha256(path):
+    """Compute the sha256 checksum of the given path.
+
+    Avoids reading the whole file in RAM, and computes the sha256 in chunks.
+
+    Parameters
+    ----------
+    path: str or file object
+        If a string, assumed to be the path to the file to be checksumed. If a
+        file object, checksum will start at the current file position.
+    """
+    return _compute_hash(path, hashlib.sha256())
+
+
+def _compute_hash(path, m):
     block_size = 256 * 1024
-    m = hashlib.md5()
 
     def _compute_checksum(fp):
         while True:
