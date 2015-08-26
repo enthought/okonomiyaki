a1a29e58f2f8ae39df7d7c188322387f7a5f190d
David Cournapeau <cournape@gmail.com>
2015-08-26 15:12:28 +0100

    MAINT: remove dependency on six.

--- okonomiyaki/utils/tests/test_misc.py
+++ okonomiyaki/utils/tests/test_misc.py
@@ -1,10 +1,9 @@
 import textwrap
 import unittest
 
-from six import StringIO
-
 from ...errors import OkonomiyakiError
 from ..misc import parse_assignments
+from ..py3compat import StringIO
 
 
 class TestParseAssignments(unittest.TestCase):
