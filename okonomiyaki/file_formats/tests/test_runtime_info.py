9d5ccb2a51048175d8f7991dd5881caf6b7344e0
David Cournapeau <cournape@gmail.com>
2015-09-08 11:23:50 +0100

    TST: add dummy runtimes for windows testing.

--- okonomiyaki/file_formats/tests/test_runtime_info.py
+++ okonomiyaki/file_formats/tests/test_runtime_info.py
@@ -1,8 +1,10 @@
 import os.path
+import sys
 import unittest
 
 from okonomiyaki.utils.test_data import (
-    JULIA_DEFAULT_0_3_11_RH5_64, PYTHON_CPYTHON_2_7_10_RH5_64,
+    JULIA_DEFAULT_0_3_11_RH5_64, JULIA_DEFAULT_0_3_11_WIN_64,
+    PYTHON_CPYTHON_2_7_10_RH5_64, PYTHON_CPYTHON_2_7_10_WIN_64
 )
 from ..runtime_metadata import runtime_metadata_factory
 from ..runtime_info import runtime_info_from_metadata, runtime_info_from_json
@@ -11,10 +13,17 @@ from ..runtime_info import runtime_info_from_metadata, runtime_info_from_json
 class TestPythonRuntimeInfoV1(unittest.TestCase):
     def test_simple(self):
         # Given
-        path = PYTHON_CPYTHON_2_7_10_RH5_64
-        metadata = runtime_metadata_factory(path)
         name = u"test"
         prefix = os.path.abspath(os.path.join(u"foo", u"bar"))
+
+        if sys.platform == "win32":
+            path = PYTHON_CPYTHON_2_7_10_WIN_64
+            r_executable = os.path.join(prefix, "python.exe")
+        else:
+            path = PYTHON_CPYTHON_2_7_10_RH5_64
+            r_executable = os.path.join(prefix, "bin", "python")
+
+        metadata = runtime_metadata_factory(path)
 
         # When
         runtime_info = runtime_info_from_metadata(metadata, prefix, name)
@@ -22,10 +31,7 @@ class TestPythonRuntimeInfoV1(unittest.TestCase):
         # Then
         self.assertEqual(runtime_info.prefix, prefix)
         self.assertEqual(runtime_info.name, name)
-        self.assertEqual(
-            runtime_info.executable,
-            os.path.join(prefix, "bin", "python")
-        )
+        self.assertEqual(runtime_info.executable, r_executable)
 
     def test_json_round_trip(self):
         # Given
@@ -46,10 +52,17 @@ class TestPythonRuntimeInfoV1(unittest.TestCase):
 class TestJuliaRuntimeInfoV1(unittest.TestCase):
     def test_simple(self):
         # Given
-        path = JULIA_DEFAULT_0_3_11_RH5_64
-        metadata = runtime_metadata_factory(path)
         name = u"test"
         prefix = os.path.abspath(os.path.join(u"foo", u"bar"))
+
+        if sys.platform == "win32":
+            path = JULIA_DEFAULT_0_3_11_WIN_64
+            r_executable = os.path.join(prefix, "bin", "julia.exe")
+        else:
+            path = JULIA_DEFAULT_0_3_11_RH5_64
+            r_executable = os.path.join(prefix, "bin", "julia")
+
+        metadata = runtime_metadata_factory(path)
 
         # When
         runtime_info = runtime_info_from_metadata(metadata, prefix, name)
@@ -57,10 +70,7 @@ class TestJuliaRuntimeInfoV1(unittest.TestCase):
         # Then
         self.assertEqual(runtime_info.prefix, prefix)
         self.assertEqual(runtime_info.name, name)
-        self.assertEqual(
-            runtime_info.executable,
-            os.path.join(prefix, "bin", "julia")
-        )
+        self.assertEqual(runtime_info.executable, r_executable)
 
     def test_json_round_trip(self):
         # Given
