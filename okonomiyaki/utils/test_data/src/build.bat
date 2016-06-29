f3b208db8a671f492b800b10296ad9cc578f0e8b
David Cournapeau <cournape@gmail.com>
2016-06-29 14:46:08 +0100

    BUG: fix bitness/output of dummy windows runtimes.

---
+++ okonomiyaki/utils/test_data/src/build.bat
@@ -0,0 +1,4 @@
+cl /Os python.c /D PYTHON_VERSION=\"2.7.9\" /Fepython_279.exe
+cl /Os python.c /D PYTHON_VERSION=\"2.7.10\" /Fepython_2710.exe
+cl /Os python.c /D PYTHON_VERSION=\"3.4.1\" /Fepython_341.exe
+cl /Os python.c /D PYTHON_VERSION=\"3.5.1\" /Fepython_351.exe
