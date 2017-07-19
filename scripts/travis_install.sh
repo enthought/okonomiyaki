68bf0e523f2f534ad2c3b35c818768d00c0bb15c
Ioannis Tziakos <ioannist@enthought.com>
2017-07-19 11:59:21 +0100

    Merge pull request #286 from enthought/maint/update-ci-setup
    MAINT/update ci setup
    (cherry picked from commit 43cf93443552b185e90f1c388f389303d92dac1a)

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -2,13 +2,5 @@
 set -e
 
 pip install -U pip
-
-if [ "${TRAVIS_PYTHON_VERSION}" == "2.6" ] || \
-    [ "${TRAVIS_PYTHON_VERSION}" == "2.7" ] || \
-    [ "${TRAVIS_PYTHON_VERSION}" == "pypy" ]; then
-    pip install -rdev_requirements2.txt;
-else
-    pip install -rdev_requirements.txt;
-fi
-
+pip install -rdev_requirements.txt;
 pip install .
