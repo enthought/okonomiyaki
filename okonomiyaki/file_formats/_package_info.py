fcc406d37c0caf55588f12c949e05807b412bd78
Kevin Kihneman <kkihneman@enthought.com>
2019-03-02 22:54:35 -0600

    Change duplicate description definition to a warning

--- okonomiyaki/file_formats/_package_info.py
+++ okonomiyaki/file_formats/_package_info.py
@@ -5,6 +5,7 @@ We support 1.0, 1.1, 1.2 and 2.0.
 """
 import contextlib
 import os.path
+import warnings
 
 import zipfile2
 
@@ -187,10 +188,11 @@ class PackageInfo(object):
             msg_body = msg.get_payload()
             if msg_body is not None and msg_body != '':
                 if 'description' in kw:
-                    raise ValueError(
+                    warnings.warn(
                         'Description defined with header and in body '
                         'of metadata.\nDescription header: "{}"\nBody: "{}"'
-                        .format(kw['description'], msg_body)
+                        .format(kw['description'], msg_body),
+                        RuntimeWarning
                     )
                 else:
                     kw['description'] = msg_body
@@ -256,48 +258,39 @@ class PackageInfo(object):
         self._write_field(s, 'Author', self.author)
         self._write_field(s, 'Author-email', self.author_email)
 
-        # Maintainer
         if metadata_version_info >= (1, 2):
             if self.maintainer:
                 self._write_field(s, 'Maintainer', self.maintainer)
             if self.maintainer_email:
                 self._write_field(s, 'Maintainer-email', self.maintainer_email)
 
-        # License
         if self.license:
             self._write_field(s, 'License', self.license)
         else:
             self._write_field(s, 'License', "UNKNOWN")
 
-        # Download-URL
         if metadata_version_info >= (1, 1):
             if self.download_url:
                 self._write_field(s, 'Download-URL', self.download_url)
 
-        # Project-URL
         if metadata_version_info >= (1, 2):
             self._write_list(s, 'Project-URL', self.project_urls)
 
-        # Description
         description = _rfc822_escape(self.description)
         self._write_field(s, 'Description', description)
 
-        # Keywords
         keywords = ' '.join(self.keywords)
         if keywords:
             self._write_field(s, 'Keywords', keywords)
 
-        # Platform
         if len(self.platforms) == 0:
             self._write_list(s, 'Platform', ("UNKNOWN",))
         else:
             self._write_list(s, 'Platform', self.platforms)
 
-        # Classifier
         if metadata_version_info >= (1, 1):
             self._write_list(s, 'Classifier', self.classifiers)
 
-        # Requires-*, Provides-*, Obsoletes-*
         if metadata_version_info == (1, 1):
             # These fields are deprecated in 1.2
             # and changed to the corresponding field names in section below.
@@ -313,7 +306,6 @@ class PackageInfo(object):
             self._write_list(s, 'Provides-Dist', self.provides_dist)
             self._write_list(s, 'Obsoletes-Dist', self.obsoletes_dist)
 
-        # New to 2.1
         if metadata_version_info >= (2, 1):
             if self.description_content_type:
                 self._write_field(s, 'Description-Content-Type', self.description_content_type)
