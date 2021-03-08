7e9245a5461ada8f16b2e18e57ab1e777c6565c0
Ioannis Tziakos <ioannist@enthought.com>
2021-03-08 16:11:01 +0000

    Update dummy python source
    - Use c++
    - Return a second line info that is expected by EDM

--- okonomiyaki/utils/test_data/src/build.bat
+++ okonomiyaki/utils/test_data/src/build.bat
@@ -1,15 +1,14 @@
-cl /c /TP /MT /O1 /GS- /Oi- python.c /D PYTHON_VERSION=\"2.7.9\" /Fopython_279.obj
-link /NODEFAULTLIB /ENTRY:main python_279.obj user32.lib kernel32.lib
+rem %1 should be 64bit or 32bit dependening on the python bitness we are building for
+
+cl -nologo -EHsc -GR -Zc:forScope -Zc:wchar_t /D PYTHON_VERSION=\"2.7.9\" /D PYTHON_ARCH=\"%1\" /Fopython_279.obj python.cpp
 call python_279.exe
-cl /c /TP /MT /O1 /GS- /Oi- python.c /D PYTHON_VERSION=\"2.7.10\" /Fopython_2710.obj
-link /NODEFAULTLIB /ENTRY:main python_2710.obj user32.lib kernel32.lib
+cl -nologo -EHsc -GR -Zc:forScope -Zc:wchar_t /D PYTHON_VERSION=\"2.7.10\" /D PYTHON_ARCH=\"%1\" /Fopython_2710.obj python.cpp
 call python_2710.exe
-cl /c /MT /TP /O1 /GS- /Oi- python.c /D PYTHON_VERSION=\"3.4.1\" /Fopython_341.obj
-link /NODEFAULTLIB /ENTRY:main python_341.obj user32.lib kernel32.lib
+cl -nologo -EHsc -GR -Zc:forScope -Zc:wchar_t /D PYTHON_VERSION=\"3.4.1\" /D PYTHON_ARCH=\"%1\" /Fopython_341.obj python.cpp
 call python_341.exe
-cl /c /MT /TP /O1 /GS- /Oi- python.c /D PYTHON_VERSION=\"3.5.1\" /Fopython_351.obj
-link /NODEFAULTLIB /ENTRY:main python_351.obj user32.lib kernel32.lib
+cl -nologo -EHsc -GR -Zc:forScope -Zc:wchar_t /D PYTHON_VERSION=\"3.5.1\" /D PYTHON_ARCH=\"%1\" /Fopython_351.obj python.cpp
 call python_351.exe
-cl /c /MT /TP /O1 /GS- /Oi- python.c /D PYTHON_VERSION=\"3.6.5\" /Fopython_365.obj
-link /NODEFAULTLIB /ENTRY:main python_365.obj user32.lib kernel32.lib
+cl -nologo -EHsc -GR -Zc:forScope -Zc:wchar_t /D PYTHON_VERSION=\"3.6.5\" /D PYTHON_ARCH=\"%1\" /Fopython_365.obj python.cpp
 call python_365.exe
+cl -nologo -EHsc -GR -Zc:forScope -Zc:wchar_t /D PYTHON_VERSION=\"3.8.8\" /D PYTHON_ARCH=\"%1\" /Fopython_388.obj python.cpp
+call python_388.exe
