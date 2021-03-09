879555f1f8051231d623f03c9c4e7f2fee506ee5
Ioannis Tziakos <ioannist@enthought.com>
2021-03-09 11:57:42 +0000

    Update default_abi to better support python 3.8

--- okonomiyaki/platforms/tests/test_abi.py
+++ okonomiyaki/platforms/tests/test_abi.py
@@ -1,46 +1,48 @@
 import sys
 
-from ...errors import OkonomiyakiError
-from ..abi import default_abi
-
 if sys.version_info < (2, 7):
     import unittest2 as unittest
 else:
     import unittest
 
+from hypothesis import given
+from hypothesis.strategies import sampled_from
+
+from ...errors import OkonomiyakiError
+from ..abi import default_abi
+
+
+EXAMPLE_RUNTIMES = {
+    ("osx_x86_64", "cpython", "2.7.10+1"): u"darwin",
+    ("rh5_x86_64", "cpython", "2.7.10+1"): u"gnu",
+    ("win_x86", "cpython", "2.7.10+1"): u"msvc2008",
+    ("win_x86", "cpython", "3.4.3+1"): u"msvc2010",
+    ("win_x86", "cpython", "3.5.0+1"): u"msvc2015",
+    ("win_x86", "cpython", "3.6.0+1"): u"msvc2015",
+    ("win_x86", "cpython", "3.8.8+1"): u"msvc2019",
+    ("osx_x86_64", "pypy", "2.6.1+1"): u"darwin",
+    ("rh5_x86_64", "pypy", "2.6.1+1"): u"gnu",
+    ("win_x86", "pypy", "2.6.1+1"): u"msvc2008",
+    ("osx_x86_64", "julia", "0.3.11+1"): u"darwin",
+    ("rh5_x86_64", "julia", "0.3.11+1"): u"gnu",
+    ("win_x86", "julia", "0.3.11+1"): u"mingw"}
+
 
 class TestDefaultABI(unittest.TestCase):
-    def test_basics(self):
-        # Given
-        args = (
-            (("osx_x86_64", "cpython", "2.7.10+1"), u"darwin"),
-            (("rh5_x86_64", "cpython", "2.7.10+1"), u"gnu"),
-            (("win_x86", "cpython", "2.7.10+1"), u"msvc2008"),
-            (("win_x86", "cpython", "3.4.3+1"), u"msvc2010"),
-            (("win_x86", "cpython", "3.5.0+1"), u"msvc2015"),
-            (("win_x86", "cpython", "3.6.0+1"), u"msvc2015"),
-            (("osx_x86_64", "pypy", "2.6.1+1"), u"darwin"),
-            (("rh5_x86_64", "pypy", "2.6.1+1"), u"gnu"),
-            (("win_x86", "pypy", "2.6.1+1"), u"msvc2008"),
-            (("osx_x86_64", "julia", "0.3.11+1"), u"darwin"),
-            (("rh5_x86_64", "julia", "0.3.11+1"), u"gnu"),
-            (("win_x86", "julia", "0.3.11+1"), u"mingw"),
-        )
 
-        # When/Then
-        for arg, r_abi in args:
-            abi = default_abi(*arg)
-            self.assertEqual(abi, r_abi)
+    @given(sampled_from(sorted(EXAMPLE_RUNTIMES)))
+    def test_basics(self, arguments):
+        # when
+        abi = default_abi(*arguments)
 
-    def test_non_supported(self):
-        # Given
-        args = (
-            ("win_x86", "cpython", "3.7.0+1"),
+        # then
+        self.assertEqual(abi, EXAMPLE_RUNTIMES[arguments])
+
+    @given(
+        sampled_from([
+            ("win_x86", "cpython", "3.9.0+1"),
             ("win_x86", "pypy", "4.1.0+1"),
-            ("rh5_x86_64", "r", "3.0.0+1"),
-        )
-
-        # When/Then
-        for arg in args:
-            with self.assertRaises(OkonomiyakiError):
-                default_abi(*arg)
+            ("rh5_x86_64", "r", "3.0.0+1")]))
+    def test_non_supported(self, arguments):
+        with self.assertRaises(OkonomiyakiError):
+            default_abi(*arguments)
