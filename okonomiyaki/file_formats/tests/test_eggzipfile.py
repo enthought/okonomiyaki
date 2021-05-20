331932765cc6e6b79852d3afe671a22834259088
Kevin Kihneman <kkihneman@enthought.com>
2021-05-20 14:15:32 -0500

    Move testing to test_eggzipfile factoring out pyc assertions

---
+++ okonomiyaki/file_formats/tests/test_eggzipfile.py
@@ -0,0 +1,86 @@
+import glob
+import importlib
+import io
+import os
+import shutil
+import sys
+import tempfile
+import unittest
+import zipfile2
+
+from ..eggzipfile import EggZipFile
+
+from .common import DUMMY_PKG_VALID_EGG
+
+
+class TestEggZipFile(unittest.TestCase):
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.tmpdir)
+
+    def assertPycValid(self, pyc_file):
+        py_file = importlib.util.source_from_cache(pyc_file)
+        statinfo = os.stat(py_file)
+        try:
+            importlib._bootstrap_external._validate_bytecode_header(
+                io.FileIO(pyc_file, 'rb').read(),
+                source_stats={'mtime': statinfo.st_mtime},
+                path=pyc_file, name=os.path.basename(pyc_file)
+            )
+        except ImportError as e:
+            self.fail(e.msg)
+
+    def assertPycInvalid(self, pyc_file):
+        py_file = importlib.util.source_from_cache(pyc_file)
+        statinfo = os.stat(py_file)
+        with self.assertRaises(ImportError):
+            importlib._bootstrap_external._validate_bytecode_header(
+                io.FileIO(pyc_file, 'rb').read(),
+                source_stats={'mtime': statinfo.st_mtime},
+                path=pyc_file, name=os.path.basename(pyc_file)
+            )
+
+    @unittest.skipIf(
+        sys.version_info.major == 2, 'importlib is different in Python 2'
+    )
+    def test_valid_pyc_egg_with_zipfile2(self):
+        # Given
+        egg = DUMMY_PKG_VALID_EGG
+
+        # When
+        with zipfile2.ZipFile(egg) as zip:
+            zip.extractall(self.tmpdir)
+
+        # Then
+        pyc_file = glob.glob(os.path.join(self.tmpdir, '**', '*.pyc'))[0]
+        self.assertPycInvalid(pyc_file)
+
+    @unittest.skipIf(
+        sys.version_info.major == 2, 'importlib is different in Python 2'
+    )
+    def test_valid_pyc_egg_with_eggzipfile_default(self):
+        # Given
+        egg = DUMMY_PKG_VALID_EGG
+
+        # When
+        with EggZipFile(egg) as zip:
+            zip.extractall(self.tmpdir)
+
+        # Then
+        pyc_file = glob.glob(os.path.join(self.tmpdir, '**', '*.pyc'))[0]
+        self.assertPycInvalid(pyc_file)
+
+    @unittest.skipIf(
+        sys.version_info.major == 2, 'importlib is different in Python 2'
+    )
+    def test_valid_pyc_egg_with_eggzipfile_force(self):
+        # Given
+        egg = DUMMY_PKG_VALID_EGG
+
+        # When
+        with EggZipFile(egg) as zip:
+            zip.extractall(self.tmpdir, force_valid_pyc_files=True)
+
+        # Then
+        pyc_file = glob.glob(os.path.join(self.tmpdir, '**', '*.pyc'))[0]
+        self.assertPycValid(pyc_file)
