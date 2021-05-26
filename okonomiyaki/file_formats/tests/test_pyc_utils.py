3191008215220e0fec0fb1524603e99a998ebdfd
Kevin Kihneman <kkihneman@enthought.com>
2021-05-26 01:00:07 -0500

    Use hypothesis to run pyc_utils test for all eggs

--- okonomiyaki/file_formats/tests/test_pyc_utils.py
+++ okonomiyaki/file_formats/tests/test_pyc_utils.py
@@ -1,15 +1,27 @@
 import glob
 import os
 import shutil
+import sys
 import tempfile
 import unittest
 import zipfile2
 
+from hypothesis import given
+from hypothesis.strategies import sampled_from
+
 from ..pyc_utils import (
     validate_bytecode_header, force_valid_pyc_file, source_from_cache
 )
+from .common import (
+    DUMMY_PKG_STALE_EGG_36, DUMMY_PKG_STALE_EGG_35, DUMMY_PKG_STALE_EGG_27
+)
 
-from .common import DUMMY_PKG_STALE_EGG_36
+
+TARGET_VERSION_TO_STALE_EGGS = {
+    (2, 7): DUMMY_PKG_STALE_EGG_27,
+    (3, 5): DUMMY_PKG_STALE_EGG_35,
+    (3, 6): DUMMY_PKG_STALE_EGG_36,
+}
 
 
 class TestPycUtils(unittest.TestCase):
@@ -17,28 +29,50 @@ class TestPycUtils(unittest.TestCase):
         self.tmpdir = tempfile.mkdtemp()
         self.addCleanup(shutil.rmtree, self.tmpdir)
 
-    def assertPycValid(self, pyc_file):
-        py_file = source_from_cache(pyc_file, (3,))
+    def execute_example(self, f):
+        """Hypothesis custom function execution to allow a tmpdir with each
+           execution of a given hypothesis value
+           (self.tmpdir doesn't change between hypothesis executions.)
+        """
+        self.hypothesis_tmpdir = tempfile.mkdtemp()
+        f()
+        shutil.rmtree(self.hypothesis_tmpdir)
+
+
+    def assertPycValid(self, pyc_file, target_version_info):
+        py_file = source_from_cache(pyc_file, target_version_info)
         try:
-            validate_bytecode_header(py_file, pyc_file, (3, 6))
+            validate_bytecode_header(py_file, pyc_file, target_version_info)
         except ImportError as e:
             self.fail(str(e))
 
-    def assertPycInvalid(self, pyc_file):
+    def assertPycInvalid(self, pyc_file, target_version_info):
         with self.assertRaises(AssertionError):
-            self.assertPycValid(pyc_file)
+            self.assertPycValid(pyc_file, target_version_info)
 
-    def test_force_valid_pyc_file(self):
+    @given(sampled_from([(2, 7), (3, 5), (3, 6)]))
+    def test_force_valid_pyc_file(self, target_version_info):
         # Given
-        egg = DUMMY_PKG_STALE_EGG_36
+        egg = TARGET_VERSION_TO_STALE_EGGS[target_version_info]
         with zipfile2.ZipFile(egg) as zip:
-            zip.extractall(self.tmpdir)
-        pyc_file = glob.glob(os.path.join(self.tmpdir, '**', '*.pyc'))[0]
-        py_file = source_from_cache(pyc_file, (3,))
-        self.assertPycInvalid(pyc_file)
+            zip.extractall(self.hypothesis_tmpdir)
+
+        if sys.version_info.major == 3:
+            search_path = os.path.join(self.hypothesis_tmpdir, '**', '*.pyc')
+            pyc_file = glob.glob(search_path, recursive=True)[0]
+        else:
+            if target_version_info[0] == 3:
+                search_path = os.path.join(
+                    self.hypothesis_tmpdir, '__pycache__', '*.pyc'
+                )
+            else:
+                search_path = os.path.join(self.hypothesis_tmpdir, '*.pyc')
+            pyc_file = glob.glob(search_path)[0]
+        py_file = source_from_cache(pyc_file, target_version_info)
+        self.assertPycInvalid(pyc_file, target_version_info)
 
         # When
-        force_valid_pyc_file(py_file, pyc_file, (3, 6))
+        force_valid_pyc_file(py_file, pyc_file, target_version_info)
 
         # Then
-        self.assertPycValid(pyc_file)
+        self.assertPycValid(pyc_file, target_version_info)
