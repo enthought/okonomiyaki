f49c97cac684d7ad9e65a91325c62714afa628fe
Kevin Kihneman <kkihneman@enthought.com>
2021-05-31 22:14:23 -0500

    Add tests for validate_bytecode_header

--- okonomiyaki/file_formats/tests/test_pyc_utils.py
+++ okonomiyaki/file_formats/tests/test_pyc_utils.py
@@ -5,6 +5,7 @@ import shutil
 import tempfile
 import unittest
 import zipfile2
+from datetime import datetime, timezone
 
 from hypothesis import given
 from hypothesis.strategies import sampled_from
@@ -14,11 +15,18 @@ from ..pyc_utils import (
     source_from_cache, get_pyc_files
 )
 from .common import (
-    DUMMY_PKG_STALE_EGG_27, DUMMY_PKG_STALE_EGG_35, DUMMY_PKG_STALE_EGG_36,
-    DUMMY_PKG_STALE_EGG_38
+    DUMMY_PKG_VALID_EGG_27, DUMMY_PKG_VALID_EGG_35, DUMMY_PKG_VALID_EGG_36,
+    DUMMY_PKG_VALID_EGG_38, DUMMY_PKG_STALE_EGG_27, DUMMY_PKG_STALE_EGG_35,
+    DUMMY_PKG_STALE_EGG_36, DUMMY_PKG_STALE_EGG_38,
 )
 
 
+EGG_PYTHON_TO_VALID_EGGS = {
+    u'2.7': DUMMY_PKG_VALID_EGG_27,
+    u'3.5': DUMMY_PKG_VALID_EGG_35,
+    u'3.6': DUMMY_PKG_VALID_EGG_36,
+    u'3.8': DUMMY_PKG_VALID_EGG_38,
+}
 EGG_PYTHON_TO_STALE_EGGS = {
     u'2.7': DUMMY_PKG_STALE_EGG_27,
     u'3.5': DUMMY_PKG_STALE_EGG_35,
@@ -49,6 +57,55 @@ class TestPycUtils(unittest.TestCase):
             validate_bytecode_header(py_file, pyc_file, egg_python)
         except ValueError as e:
             self.fail(str(e))
+
+    @given(sampled_from([u'2.7', u'3.5', u'3.6', u'3.8']))
+    def test_validate_bytecode_header_valid(self, egg_python):
+        # Given
+        valid_egg = EGG_PYTHON_TO_VALID_EGGS[egg_python]
+        with zipfile2.ZipFile(valid_egg) as zip:
+            py_files = [
+                f for f in zip.namelist() if f.endswith('.py')
+            ]
+            py_file = py_files[0]
+            zip_info = zip.getinfo(py_file)
+            py_path = zip.extract(zip_info, self.hypothesis_tmpdir)
+            ts = datetime(*zip_info.date_time, tzinfo=timezone.utc).timestamp()
+            os.utime(py_path, (ts, ts))
+
+            pyc_file = cache_from_source(py_file, egg_python)
+            if os.path.sep == '\\':
+                pyc_file = pyc_file.replace('\\', '/')
+            pyc_path = zip.extract(pyc_file, self.hypothesis_tmpdir)
+
+        # When/Then
+        try:
+            validate_bytecode_header(py_path, pyc_path, egg_python)
+        except ValueError as e:
+            self.fail(str(e))
+
+
+    @given(sampled_from([u'2.7', u'3.5', u'3.6', u'3.8']))
+    def test_validate_bytecode_header_stale(self, egg_python):
+        # Given
+        stale_egg = EGG_PYTHON_TO_STALE_EGGS[egg_python]
+        with zipfile2.ZipFile(stale_egg) as zip:
+            py_files = [
+                f for f in zip.namelist() if f.endswith('.py')
+            ]
+            py_file = py_files[0]
+            zip_info = zip.getinfo(py_file)
+            py_path = zip.extract(zip_info, self.hypothesis_tmpdir)
+            ts = datetime(*zip_info.date_time, tzinfo=timezone.utc).timestamp()
+            os.utime(py_path, (ts, ts))
+
+            pyc_file = cache_from_source(py_file, egg_python)
+            if os.path.sep == '\\':
+                pyc_file = pyc_file.replace('\\', '/')
+            pyc_path = zip.extract(pyc_file, self.hypothesis_tmpdir)
+
+        # When/Then
+        with self.assertRaises(ValueError):
+            validate_bytecode_header(py_path, pyc_path, egg_python)
 
     @given(sampled_from([u'2.7', u'3.5', u'3.6', u'3.8']))
     def test_force_valid_pyc_file(self, egg_python):
