7c4e7df01c57268deb14cc9a2035085f914c4395
Ioannis Tziakos <ioannist@enthought.com>
2021-06-30 13:55:12 +0100

    Use packaging.tags to verify the pep425 module

--- okonomiyaki/platforms/tests/test_pep425.py
+++ okonomiyaki/platforms/tests/test_pep425.py
@@ -1,79 +1,77 @@
 import sys
 import unittest
 
-try:
-    from pip import pep425tags as pip_pep425
-    HAS_PIP_PEP425 = True
-except ImportError as e:
-    print(e)
-    HAS_PIP_PEP425 = False
+from packaging import tags
 
 from ..pep425 import compute_abi_tag, compute_python_tag, compute_platform_tag
 
+def _system_tags():
+    interp_name = tags.interpreter_name()
+    if interp_name == "cp":
+        yield from tags.cpython_tags()
+    else:
+        yield from tags.generic_tags()
 
-@unittest.skipIf(
-    not HAS_PIP_PEP425, "Could not import pip.pep425 for comparison"
-)
 class TestPEP425(unittest.TestCase):
     def test_abi_tag(self):
         # Given
         executable = sys.executable
-        r_abi_tag = pip_pep425.get_abi_tag()
+        tag = next(_system_tags())
 
         # When
         abi_tag = compute_abi_tag(executable)
 
         # Then
-        self.assertEqual(abi_tag, r_abi_tag)
+        self.assertEqual(abi_tag, tag.abi)
 
     def test_abi_tag_default(self):
         # Given
-        r_abi_tag = pip_pep425.get_abi_tag()
+        tag = next(_system_tags())
 
         # When
         abi_tag = compute_abi_tag()
 
         # Then
-        self.assertEqual(abi_tag, r_abi_tag)
+        self.assertEqual(abi_tag, tag.abi)
 
     def test_python_tag(self):
         # Given
         executable = sys.executable
-        r_python_tag = pip_pep425.get_impl_tag()
+        tag = next(_system_tags())
 
         # When
         python_tag = compute_python_tag(executable)
 
         # Then
-        self.assertEqual(python_tag, r_python_tag)
+        self.assertEqual(python_tag, tag.interpreter)
 
     def test_python_tag_default(self):
         # Given
-        r_python_tag = pip_pep425.get_impl_tag()
+        tag = next(_system_tags())
 
         # When
         python_tag = compute_python_tag()
 
         # Then
-        self.assertEqual(python_tag, r_python_tag)
+        self.assertEqual(python_tag, tag.interpreter)
 
     def test_platform_tag(self):
         # Given
         executable = sys.executable
-        r_platform_tag = pip_pep425.get_platform()
+        tag = next(_system_tags())
 
         # When
         platform_tag = compute_platform_tag(executable)
 
         # Then
-        self.assertEqual(platform_tag, r_platform_tag)
+        self.assertEqual(platform_tag, tag.platform)
 
     def test_platform_tag_default(self):
         # Given
-        r_platform_tag = pip_pep425.get_platform()
+        tag = next(_system_tags())
 
         # When
         platform_tag = compute_platform_tag()
 
         # Then
-        self.assertEqual(platform_tag, r_platform_tag)
+        self.assertEqual(platform_tag, tag.platform)
