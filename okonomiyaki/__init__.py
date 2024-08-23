2f2f48e7af6752b258fa3c3aa9cddef8cb179571
Ioannis Tziakos <ioannist@enthought.com>
2024-08-23 15:40:19 +0100

    Fix flake8 errors

--- okonomiyaki/__init__.py
+++ okonomiyaki/__init__.py
@@ -1,3 +1,3 @@
-#  Copyright (c) 2013 by Enthought, Inc.
+#  Copyright (c) 2013-2024 by Enthought, Inc.
 #  All rights reserved.
-from ._version import version as __version__
+from ._version import version as __version__  # noqa
