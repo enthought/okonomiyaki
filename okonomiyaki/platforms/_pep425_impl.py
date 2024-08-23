1ae4a085b2bef6159fa2812ab3dd64e120dab058
Ioannis Tziakos <ioannist@enthought.com>
2024-08-23 15:11:49 +0100

    Try to fix the pep425 output for platforms

--- okonomiyaki/platforms/_pep425_impl.py
+++ okonomiyaki/platforms/_pep425_impl.py
@@ -102,18 +102,20 @@ def _is_running_32bit():
 
 def get_platform():
     """Return our platform name 'win32', 'linux_x86_64'"""
-
     try:
         import distutils.util
     except ImportError:
-        import platform
-        result = platform.platform().lower()
+        import sysconfig
+        result = sysconfig.get_platform().string.replace(".", "_").replace("-", "_")
     else:
         result = distutils.util.get_platform().replace('.', '_').replace('-', '_')
-    if result == "linux_x86_64" and _is_running_32bit():
-        # 32 bit Python program (running on a 64 bit Linux): pip should only
-        # install and run 32 bit compiled extensions in that case.
-        result = "linux_i686"
+    if _is_running_32bit():
+       # 32 bit Python program (running on a 64 bit Linux): pip should only
+       # install and run 32 bit compiled extensions in that case.
+       if result == "linux_x86_64":
+           result = "linux_i686"
+       elif result == "linux_aarch64":
+           result = "linux_armv71"
     return result
 
 
